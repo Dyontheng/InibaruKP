4eb809f7b3a42f8083e0afde5d559b17b1cf474f
Aprol <dyonfilipo@gmail.com>
2025-06-19 21:44:26 +0700

    cases and Type Legal

--- kp-App/resources/views/layouts/LegalDept/Staff/casesType/index.blade.php
+++ kp-App/resources/views/layouts/LegalDept/Staff/casesType/index.blade.php
@@ -9,14 +9,16 @@
             <div class="card-body py-3">
                 <h1>Cases Type</h1>
 
-                 <form method="GET" action="{{ route('casesTypeStaffLegal.index') }}">
+                <form method="GET" action="{{ route('casesTypeStaffLegal.index') }}">
                     <div class="form-group mb-3">
                         <label for="filter">Filter berdasarkan Jenis Cases:</label>
                         <select name="filter" id="filter" class="form-control" onchange="this.form.submit()">
                             <option value="">-- Pilih Jenis Cases --</option>
-                            <option value="Pengadilan" {{ request('filter') == 'Pengadilan' ? 'selected' : '' }}>Pengadilan</option>
+                            <option value="Pengadilan" {{ request('filter') == 'Pengadilan' ? 'selected' : '' }}>Pengadilan
+                            </option>
                             <option value="Lahan" {{ request('filter') == 'Lahan' ? 'selected' : '' }}>Lahan</option>
-                            <option value="Operasional" {{ request('filter') == 'Operasional' ? 'selected' : '' }}>Operasional
+                            <option value="Operasional" {{ request('filter') == 'Operasional' ? 'selected' : '' }}>
+                                Operasional
                             </option>
                             <option value="Rapat" {{ request('filter') == 'Rapat' ? 'selected' : '' }}>Rapat</option>
                         </select>
@@ -46,7 +48,12 @@
                                         <td>{{ $case->site_cases }}</td>
                                         <td>{{ $case->status_cases }}</td>
                                         <td>{{ $case->info_cases }}</td>
-                                        <td>{{ $case->doc_cases }}</td>
+                                        <td>
+                                            <a href="{{ asset('storage/dokumen_cases/' . $case->doc_cases) }}"
+                                                target="_blank">
+                                                {{ basename($case->doc_cases) }}
+                                            </a>
+                                        </td>
                                     </tr>
                                 @empty
                                     <tr>
@@ -66,4 +73,3 @@
 
     </div>
 @endsection
-
